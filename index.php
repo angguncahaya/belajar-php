4c4f430180d98ac23951cdc3d03af630fdc52cc1
Anggun Cahaya Purnamasari <angguncahaya.20018@mhs.unesa.ac.id>
2022-10-24 10:59:47 +0700

    php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo"hello guys!<br>";
+
+$nama = "Anggun Cahaya Purnamasari";
+$umur = 20;
+
+echo " Nama Saya <strong>$nama</strong>, saya berusia $umur tahun. <br>";
+
+$namaAdik = "Angga Cahyo Purnomo";
+$umurAdik = 15;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>"; 
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+?>
